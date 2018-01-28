64d0c2798998b7e36521dbfe344d0d143b31bcb0
Jason <floresj4@gmail.com>
2018-01-27 21:24:09 -0500

    styling error information on firstname, lastname, and email fields

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -10,6 +10,8 @@ export class SignupComponent implements OnInit {
 
   accountTypes: string[] = ['Basic', 'Advanced', 'Pro'];
   selectedAccountType: string;
+
+  @ViewChild('firstname') fieldname;
 
   constructor() { }
 
