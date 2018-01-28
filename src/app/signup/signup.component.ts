14d6780948bc67e35f15dd6a2919fae765ad99aa
Jason <floresj4@gmail.com>
2018-01-27 21:02:08 -0500

    added email validation message and form submit() event

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -16,8 +17,8 @@ export class SignupComponent implements OnInit {
     this.selectedAccountType = 'Pro';
   }
 
-  onSubmit() {
-
+  onSubmit(signupForm: NgForm) {
+    console.log(signupForm);
   }
 
   onReset() {
