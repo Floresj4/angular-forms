fb01c6eeb1e78d3a8738163474d685a641518c24
Jason <floresj4@gmail.com>
2018-02-04 01:54:56 -0500

    created simple robot response field

--- src/app/account/security-questions/security-questions.component.ts
+++ src/app/account/security-questions/security-questions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-security-questions',
@@ -16,7 +16,7 @@ export class SecurityQuestionsComponent implements OnInit {
 
     // initialize the form
     this.securityForm = new FormGroup({
-
+      'robot': new FormControl(null, Validators.required)
     });
 
   }
