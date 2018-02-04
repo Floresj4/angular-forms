9fed39f6b08ab867fd1c8ed665392bd0f3074cc2
Jason <floresj4@gmail.com>
2018-02-04 02:11:47 -0500

    starting custom validator for robot response

--- src/app/account/security-questions/security-questions.component.ts
+++ src/app/account/security-questions/security-questions.component.ts
@@ -10,15 +10,34 @@ export class SecurityQuestionsComponent implements OnInit {
 
   securityForm: FormGroup;
 
+  validRobotResponse: string[] = ['yes', 'no', 'maybe'];
+
   constructor() { }
 
   ngOnInit() {
 
     // initialize the form
     this.securityForm = new FormGroup({
-      'robot': new FormControl(null, Validators.required)
+      'robot': new FormControl(null, [
+        Validators.required,
+        this.isValidRobotResponse.bind(this)
+      ])
     });
 
   }
 
+  isValidRobotResponse(control: FormControl): {'robotResponse': boolean} {
+    const value = control.value !== null
+      ? control.value.toLowerCase() : null;
+
+    if (this.validRobotResponse.indexOf(value) === -1) {
+      return { 'robotResponse': true };
+    }
+
+    return null;
+  }
+
+  onSubmit() {
+    console.log(this.securityForm);
+  }
 }
